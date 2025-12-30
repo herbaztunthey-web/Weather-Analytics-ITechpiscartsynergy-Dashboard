6d3da36b8792456b875fbcfe255d4f735c211059
herbaztunthey-web <herbaztunthey@gmail.com>
2025-12-30 06:52:38 +0400

    Final App Build

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from datetime import datetime, timezone, timedelta
 import requests
-from flask import Flask, render_template, request, session, Response, redirect, url_for
+from flask import Flask, render_template, request, session, Response, redirect, url_for, send_from_directory
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
-app.secret_key = 'babatunde_premium_v8'
+app.secret_key = 'babatunde_app_pro_2025'
 
 API_KEY = os.getenv('WEATHER_API_KEY')
+
+# --- PWA SERVICE ROUTES ---
+
+
+@app.route('/manifest.json')
+def serve_manifest():
+    return send_from_directory('static', 'manifest.json')
+
+
+@app.route('/sw.js')
+def serve_sw():
+    return send_from_directory('static', 'sw.js')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -28,7 +40,7 @@ def index():
                 if response.get('cod') == 200:
                     weather_data = {
                         'city': response['name'],
-                        'temp': response['main']['temp'],
+                        'temp': round(response['main']['temp'], 1),
                         'humidity': response['main']['humidity'],
                         'desc': response['weather'][0]['description'],
                         'icon': response['weather'][0]['icon'],
@@ -56,17 +68,6 @@ def index():
 def clear_session():
     session.clear()
     return redirect(url_for('index'))
-
-
-@app.route('/download')
-def download_report():
-    results = session.get('last_results', [])
-    if not results:
-        return "No data", 400
-    report_content = f"WEATHER ANALYSIS - BABATUNDE ABASS\n" + "="*40 + "\n"
-    for w in results:
-        report_content += f"{w['city']}: {w['temp']}°C | {w['desc']}\n"
-    return Response(report_content, mimetype="text/plain", headers={"Content-disposition": "attachment; filename=report.txt"})
 
 
 if __name__ == '__main__':
